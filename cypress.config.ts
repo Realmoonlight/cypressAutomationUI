4918c790dee6bc57b291392333ef43e77f9ab9e0
realmoonlight <realmoonlight83@yahoo.com>
2024-02-27 14:16:43 -0500

    add git folder

--- cypress.config.ts
+++ cypress.config.ts
@@ -1,19 +1,38 @@
-import { defineConfig } from "cypress"; 
+import { defineConfig } from "cypress";
+
+const xlsx = require("node-xlsx").default;
+const fs = require("fs"); // for file
+const path = require("path")
 
 export default defineConfig({
   e2e: {
     baseUrl: "https://uitestingplayground.com",
     setupNodeEvents(on, config) {
       // implement node event listeners here
+      // reading Excel document from fixre
+      on("task", {
+        parseXlsx({ filePath }) {
+          return new Promise((resolve, reject) => {
+            try {
+              const jsonData = xlsx.parse(fs.readFileSync(filePath));
+              resolve(jsonData);
+            } catch (e) {
+              reject(e);
+            }
+          });
+        },
+      });
     },
     env:{
-      stage: 'https://stage.pasv.us/cours',
+      stage: 'https://stage.pasv.us',
       prod: 'https://coding.pasv.us/cours',
       test: 'Hello World!',
       base: 'https://uitestingplayground.com',
       play1: 'https://play1.automationcamp.ir/expected_conditions.html',
       demoQA:'https://demoqa.com',
-      herokuapp: 'https://the-internet.herokuapp.com'
+      herokuapp: 'https://the-internet.herokuapp.com',
+      email: "realmoonlight83@yahoo.com",
+      password:'Dylan2010'
       },
   },
 defaultCommandTimeout: 17_000, 
--- cypress/e2e/iframe.cy.ts
+++ cypress/e2e/iframe.cy.ts
@@ -1,5 +1,6 @@
 import { IFramePage } from "../../pages/IFrame";
 import { IFrameAppPage } from "../../pages/IframeApp";
+
 describe("IFRAME", () => {
   beforeEach(() => {
     IFramePage.visit();
@@ -18,5 +19,24 @@ describe.only("IFRAMEAPP", () => {
   it("test iframe with extention", () => {
     IFrameAppPage.getIframeApp();
   
+  });
+});
+
+describe.only('IFRAME WiTH PLUGIN', () => {
+  beforeEach(() =>{
+    cy.visit(`${Cypress.env('herokuapp')}/iframe`)
+  })
+  it('test iFrame', () => {
+    cy.frameLoaded('#mce_0_ifr')
+    cy.iframe('#mce_0_ifr').then((bodyWork) =>{
+      cy.wrap(bodyWork).type('{selectAll}{del}').type('Cypress!')
+      cy.wrap(bodyWork).should('have.text', 'Cypress!')
+      cy.wrap(bodyWork).clear().type('Hello world!')
+      cy.then(() => {
+        expect(bodyWork.text()).to.eq('Hello world!')
+      }) 
+      
+
+    })
   });
 });
